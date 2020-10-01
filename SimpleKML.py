75a3b45a33447276199cf9ca89c41a11975408f3
pkapeel <pkap@tutanota.com>
2020-09-30 20:23:07 -0500

    Started to filter out lines.

--- SimpleKML.py
+++ SimpleKML.py
@@ -1,7 +1,3 @@
-# keep the typical xml heading information
-# read the file line by line
-# determine which lines to delete and which lines to keep
-
 # CREATE A COPY OF THE .KML FILE WHICH IS TO BE USED
 import shutil
 
@@ -18,5 +14,14 @@ fin = open(SimpleKML, 'rt')
 lines = fin.readlines()
 fin.close()
 print(len(lines), 'Lines Read')
-print(lines[0])
 
+# CREATE A LIST OF OBJECTS TO KEEP
+
+keep = ['xml', 'kml', 'Document', 'name', 'Style', 'Folder', 'Placemark', 
+        'Point', 'coordinates', 'styleURL', 'color', 'LineStyle', 'width', 'LineString']
+
+# DELETE LINES THAT DO NOT CONTAIN OBJECTS TO KEEP
+
+for line in lines:
+    for feature in keep:
+        if 
